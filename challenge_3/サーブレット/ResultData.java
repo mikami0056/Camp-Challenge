c22a49df3b92a7cdd697716a5c03331a35c24464
三上祥一郎 <mikami0056@gmail.com>
2016-04-01 13:34:40 +0900

    4月1日：サーブレットの課題を提出しました。

---
+++ challenge_3/サーブレット/ResultData.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet;
+
+import java.util.Date;
+import java.io.Serializable;
+
+/**
+ *
+ * @author SHO
+ */
+public class ResultData implements Serializable{
+    private Date d;
+    private String luck;
+    
+    public ResultData() {}
+    
+    /**
+     * @return the d
+     */
+    public Date getD() {
+        return d;
+    }
+    
+    /**
+     * @param d the d to set
+     */
+    public void setD(Date d){
+        this.d = d;
+    }
+    /**
+     * @return the luck
+     */
+    public String getLuck(){
+        return luck;
+    }
+    
+    /**
+     * @param Luck the luck to set
+     */
+    public void setLuck(String Luck ){
+        this.luck = Luck;
+    }
+}

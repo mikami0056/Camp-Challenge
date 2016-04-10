09ef1500a4c9c092081db1f800f5dd8f6f5d09c4
三上祥一郎 <mikami0056@gmail.com>
2016-04-10 17:54:28 +0900

    no message

--- challenge_5/tasks.java
+++ challenge_5/tasks.java
@@ -22,9 +22,9 @@ public class tasks {
     
     void task2(){
         Date d2 = new Date();                                                   
-        SimpleDateFormat f1 = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-        String s = f1.format(d2);
-        System.out.println(s);
+        SimpleDateFormat f2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String s = f2.format(d2);
+        System.out.println("現在時刻：" + s);
     }
     
     void task3(){
@@ -38,30 +38,39 @@ public class tasks {
     }
     
     void task4(){
-        Calendar cal4j = Calendar.getInstance();        //1月用のインスタンスを生成
-        Calendar cal4d = Calendar.getInstance();        //12月用のインスタンスを生成
+        Calendar cal4j = Calendar.getInstance();                                //1月用のインスタンスを生成
+        Calendar cal4d = Calendar.getInstance();                                //12月用のインスタンスを生成
         
-        cal4j.clear();                                  //保持されている時刻を消去
-        cal4j.set(2015,1,1,0,0,0);                      //1月の時刻を設定
+        cal4j.clear();                                                          //保持されている時刻を消去
+        cal4j.set(2015,1,1,0,0,0);                                              //1月の時刻を設定
         cal4d.clear();                                  
         cal4d.set(2015,12,31,23,59,59);
         
-        long ts4j = cal4j.getTimeInMillis();            //1月のタイムスタンプを取得
-        long ts4d = cal4d.getTimeInMillis();            //12月のタイムスタンプを取得
+        long ts4j = cal4j.getTimeInMillis();                                    //1月のタイムスタンプを取得
+        long ts4d = cal4d.getTimeInMillis();                                    //12月のタイムスタンプを取得
         
-        long datediff = (ts4d - ts4j);                  //1月と12月の差分を取得
-        datediff = datediff / 1000;                     //単位変換(ms→s)用
+        long datediff = (ts4d - ts4j);                                          //1月と12月の差分を取得
+        datediff = datediff / 1000;                                             //単位変換(ms→s)用
         System.out.println(datediff);
     }
     
-    void task5(){
+    void task5() throws UnsupportedEncodingException{
         String myname = "三上祥一郎";
-        System.out.println(myname.length());
+        System.out.println("名前：三上祥一郎");
+        System.out.println("名前の文字数：" + myname.length());
+        System.out.println("名前のバイト数：" + myname.getBytes("UTF-8").length);  //UTF-8で表示するため15バイト(1文字バイト)
+
     }
     
     void task6(){
-        String address = "mikami0056@gmail.com";
-        System.out.println(address.substring(11));
+        String address = "mikami0056@gmail.com";                                
+        for(int i = 0; i < address.length(); i++){                              //文字列を検索
+            int j = address.indexOf("@");
+            if(j == i){
+                System.out.println(address.substring(i));
+            }
+        }
+        //System.out.println(address.substring(10));
     }
     
     void task7(){

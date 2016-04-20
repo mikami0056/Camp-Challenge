0188b2ca1242ef328e23b866253167008c182601
三上祥一郎 <mikami0056@gmail.com>
2016-04-20 23:07:02 +0900

    Javaによるデータベース操作の課題を更新しました。

--- challenge_8/java/task12a.java
+++ challenge_8/java/task12a.java
@@ -10,41 +10,52 @@ package dbconnect;
  * @author SHO
  */
 public class task12a {
-    
-    task12a(){
-    }
 
     private String sql;
+    private String originalSql;
+    private String nameSql;
+    private String ageSql;
+    private String birthdateSql;
     private String name, age, birthdate;
+    
+    task12a(){
+        this.originalSql = "SELECT * FROM profiles ";
+    }
     
     public void setName(String name){
         if("".equals(name)){
-            this.name = null;
-            
+            this.name = null;         
         } else {
             this.name = name;
-            
+            this.nameSql = "name LIKE \'%" + this.name + "%\'";
         }
+    }
+    public String getName(){
+        return this.name;
     }
     
     public void setAge(String age){
         if("".equals(age)){
             this.age = null;
-            
         } else {
             this.age = age;
-            
+            this.ageSql = "age LIKE " + this.age ;
         }
+    }
+    public String getAge(){
+        return this.age;
     }
     
     public void setBirthdate(String birthdate){
         if("".equals(birthdate)){
             this.birthdate = null;
-            
         } else {
             this.birthdate = birthdate;
-            
+            this.birthdateSql = "birthday LIKE \'%" + this.birthdate + "%\'";   
         }
+    }
+    public String getBirthdate(){
+        return this.birthdate;
     }
 
     public boolean confirmation (){
@@ -56,48 +67,33 @@ public class task12a {
         }
         return type;
     }
-    
-    public void setSql(String name, String age, String birthdate){
-        if(name == null || "".equals(name)){
-            this.sql = "SELECT * FROM profiles WHERE age = " + age + " OR birthday LIKE \'%" + birthdate +"%\'";
-        } else if(age == null || "".equals(age)){
-            this.sql = "SELECT * FROM profiles WHERE name LIKE \'%" + name + "%\' OR birthday LIKE \'%" + birthdate +"%\'";
-        } else if(birthdate == null || "".equals(birthdate)){
-            
-        }
-        
-        this.sql = "SELECT * FROM profiles WHERE name LIKE \'%" + name + "%\' OR age = " + age + " OR birthday LIKE \'%" + birthdate +"%\'";
-    }
-    
+  
     public String getSql(){
-        if(this.name != null && this.age != null && this.birthdate != null){
-           this.sql = "SELECT * FROM profiles WHERE name LIKE \'%" + name + "%\' OR age = " + age + " OR birthday LIKE \'%" + birthdate +"%\'";
-           
-        } else if(this.name != null){
-            //名前入力有り
-            if(this.age != null){
-                this.sql = "SELECT * FROM profiles WHERE name LIKE \'%" + name + "%\' OR age = " + age;
-            } else if (this.birthdate != null){
-                this.sql = "SELECT * FROM profiles WHERE name LIKE \'%" + name + "%\' OR birthday LIKE \'%" + birthdate +"%\'";             
-            } else {
-                this.sql = "SELECT * FROM profiles WHERE name LIKE \'%" + name + "%\'";
-            }
-            //名前入力有り
-            
-        } else if (this.age != null){
-            //名前入力無し, 年齢入力有り
-            if(this.birthdate != null){
-                this.sql = "SELECT * FROM profiles WHERE age = " + age + " OR birthday LIKE \'%" + birthdate +"%\'";                
-            } else {
-                this. sql = "SELECT * FROM profiles WHERE age = " + age;
-            }
-            //名前入力無し, 年齢入力有り
-            
-        } else {
-            //名前, 年齢入力無し
-            this.sql = "SELECT * FROM profiles WHERE  birthday LIKE \'%" + birthdate +"%\'";
-        }
+        boolean nameType = false;
+        boolean ageType = false; 
         
+        if(this.name != null){
+            //名前入力有り
+            this.sql = this.originalSql + "WHERE " + this.nameSql;
+            nameType = true;
+        } 
+        
+        if(this.age != null){
+            if(nameType == true){   //名前がセットされてる場合
+                this.sql = this.sql + " OR " + this.ageSql;
+                ageType = true;
+            } else {                //名前がセットされていな場合
+                this.sql = this.originalSql + "WHERE " + this.ageSql;
+            }
+        } 
+        
+        if(this.birthdate != null){
+            if(nameType == true || ageType == true){    //名前又は年齢がセットされている場合
+                this.sql = this.sql + " OR " + this.birthdateSql;
+            } else if(nameType == false && ageType == false){   //どちらもセットされていない場合
+                this.sql = this.originalSql + "WHERE " + this.birthdateSql;
+            }   
+        } 
         return this.sql;
     } 
 }
